afb222a6597d8ad8702e394adf2a08a0a54b5391
m&m <38250127+mohsafer@users.noreply.github.com>
2024-10-02 17:20:11 +0330

    HPC segment

--- data_factory/data_loader.py
+++ data_factory/data_loader.py
@@ -13,7 +13,7 @@ from sklearn.preprocessing import StandardScaler
 import pickle
 
 
-class PSMSegLoader(object):
+class HPCSegLoader(object):      #PSM -->HPC
     def __init__(self, data_path, win_size, step, mode="train"):
         self.mode = mode
         self.step = step
@@ -59,6 +59,51 @@ class PSMSegLoader(object):
                               index // self.step * self.win_size:index // self.step * self.win_size + self.win_size]), np.float32(
                 self.test_labels[index // self.step * self.win_size:index // self.step * self.win_size + self.win_size])
 
+class PSMSegLoader(object):      #PSM -->HPC
+    def __init__(self, data_path, win_size, step, mode="train"):
+        self.mode = mode
+        self.step = step
+        self.win_size = win_size
+        self.scaler = StandardScaler()
+        data = pd.read_csv(data_path + '/train.csv')
+        data = data.values[:, 1:]
+        data = np.nan_to_num(data)
+        self.scaler.fit(data)
+        data = self.scaler.transform(data)
+        test_data = pd.read_csv(data_path + '/test.csv')
+        test_data = test_data.values[:, 1:]
+        test_data = np.nan_to_num(test_data)
+        self.test = self.scaler.transform(test_data)
+        self.train = data
+        self.val = self.test
+        self.test_labels = pd.read_csv(data_path + '/test_label.csv').values[:, 1:]
+
+    def __len__(self):
+        """
+        Number of images in the object dataset.
+        """
+        if self.mode == "train":
+            return (self.train.shape[0] - self.win_size) // self.step + 1
+        elif (self.mode == 'val'):
+            return (self.val.shape[0] - self.win_size) // self.step + 1
+        elif (self.mode == 'test'):
+            return (self.test.shape[0] - self.win_size) // self.step + 1
+        else:
+            return (self.test.shape[0] - self.win_size) // self.win_size + 1
+
+    def __getitem__(self, index):
+        index = index * self.step
+        if self.mode == "train":
+            return np.float32(self.train[index:index + self.win_size]), np.float32(self.test_labels[0:self.win_size])
+        elif (self.mode == 'val'):
+            return np.float32(self.val[index:index + self.win_size]), np.float32(self.test_labels[0:self.win_size])
+        elif (self.mode == 'test'):
+            return np.float32(self.test[index:index + self.win_size]), np.float32(
+                self.test_labels[index:index + self.win_size])
+        else:
+            return np.float32(self.test[
+                              index // self.step * self.win_size:index // self.step * self.win_size + self.win_size]), np.float32(
+                self.test_labels[index // self.step * self.win_size:index // self.step * self.win_size + self.win_size])
 
 class MSLSegLoader(object):
     def __init__(self, data_path, win_size, step, mode="train"):
@@ -505,10 +550,12 @@ def get_loader_segment(index, data_path, batch_size, win_size=100, step=100, mod
         dataset = SMDSegLoader(data_path, win_size, 1, mode)
     elif (dataset == 'MSL'):
         dataset = MSLSegLoader(data_path, win_size, 1, mode)
-    elif (dataset == 'SMAP'):
-        dataset = SMAPSegLoader(data_path, win_size, 1, mode)
     elif (dataset == 'PSM'):
         dataset = PSMSegLoader(data_path, win_size, 1, mode)
+    elif (dataset == 'SMAP'):
+        dataset = SMAPSegLoader(data_path, win_size, 1, mode)
+    elif (dataset == 'HPC'):
+        dataset = HPCSegLoader(data_path, win_size, 1, mode)
     elif (dataset =='SWAT'):
         dataset = SWATSegLoader(data_path,win_size,1,mode)
     elif (dataset == 'UCR'):
