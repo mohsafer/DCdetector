b91dc043792d7eee07aa16c01f6f5111bfd50957
m&m <38250127+mohsafer@users.noreply.github.com>
2024-11-23 10:58:45 +0330

    gp

--- scripts/SMD.sh
+++ scripts/SMD.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES= 0,1,2,3
+export CUDA_VISIBLE_DEVICES= 0
 
 python main.py --anormly_ratio 0.6 --num_epochs 2   --batch_size 256  --mode train --dataset SMD  --data_path SMD   --input_c 38   --output_c 38  --loss_fuc MSE  --win_size 105  --patch_size 57 --devices 0,1,2,3 --use_multi_gpu
 python main.py --anormly_ratio 0.6 --num_epochs 10   --batch_size 256  --mode test    --dataset SMD   --data_path SMD     --input_c 38      --output_c 38   --loss_fuc MSE   --win_size 105  --patch_size 57 --devices 0,1,2,3 --use_multi_gpu
