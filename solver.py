1c9d25b604e6dd9cfb2d988a7d2d96ddaa894db5
m&m <38250127+mohsafer@users.noreply.github.com>
2024-11-23 10:53:38 +0330

    test

--- solver.py
+++ solver.py
@@ -310,9 +310,9 @@ class Solver(object):
             test_labels.append(labels)
 
         if len(attens_energy) > 0:
-            print("atten list:" attens_energy)
+            #print("atten list:" attens_energy)
             attens_energy = np.concatenate(attens_energy, axis=0).reshape(-1)
-            print("Concatenation successful!")
+            #print("Concatenation successful!")
         else:
             print("Error: attens_energy is empty. Cannot concatenate.")
 
