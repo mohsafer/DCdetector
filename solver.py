5513e03888f71638b860f46d8f1d2081c615eafa
m&m <38250127+mohsafer@users.noreply.github.com>
2024-12-11 22:48:12 +0330

    e

--- solver.py
+++ solver.py
@@ -169,7 +169,7 @@ class Solver(object):
                 prior_loss = prior_loss / len(prior)
 
                 loss = prior_loss - series_loss 
-                running_loss += loss.item()
+                running_loss += series_loss.item()
                 
                
              
@@ -186,7 +186,7 @@ class Solver(object):
                 #running_loss += loss.item()
                 #writer.add_scalar("Loss/train", loss.item(), epoch * len(self.train_loader) + i)
                 writer.add_scalar('training loss', running_loss / 1000, epoch * len(self.train_loader) + i)
-                print('epoch {}, loss {}'.format(epoch, loss.item()))
+                #print('epoch {}, loss {}'.format(epoch, loss.item()))
                 running_loss = 0.0
             vali_loss1, vali_loss2 = self.vali(self.test_loader)
        
