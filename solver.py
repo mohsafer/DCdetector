8cd4d07f46295e6e0b633772062465d640ad4477
m&m <38250127+mohsafer@users.noreply.github.com>
2024-12-11 19:56:29 +0330

    c

--- solver.py
+++ solver.py
@@ -182,7 +182,7 @@ class Solver(object):
 
             vali_loss1, vali_loss2 = self.vali(self.test_loader)
             # TWESTTETSTTETETSTTTSTTETETTETTETETETTEETETETETETTETEETETETETETETTETETETETETETETETETTETE
-            writer.add_scalar("Loss/train", loss, epoch)
+            writer.add_scalar("Loss/train", loss, epoch * len(self.train_loader) + i)
             print(
                 "Epoch: {0}, Cost time: {1:.3f}s ".format(
                     epoch + 1, time.time() - epoch_time))
