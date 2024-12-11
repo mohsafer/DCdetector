f04425defdad559da812bc301fcaee88bb77d537
m&m <38250127+mohsafer@users.noreply.github.com>
2024-12-11 21:41:27 +0330

    d

--- solver.py
+++ solver.py
@@ -13,7 +13,7 @@ from metrics.metrics import *
 import warnings
 warnings.filterwarnings('ignore')
 
-
+writer = SummaryWriter()
 
 def my_kl_loss(p, q):
     res = p * (torch.log(p + 0.0001) - torch.log(q + 0.0001))
@@ -64,7 +64,7 @@ class EarlyStopping:
         
 class Solver(object):
     DEFAULTS = {}
-    writer = SummaryWriter()
+    
     def __init__(self, config):
 
         self.__dict__.update(Solver.DEFAULTS, **config)
