e747e8c079e33c0b658cf5f7b1c4af3b7115e4fd
m&m <38250127+mohsafer@users.noreply.github.com>
2024-12-11 15:28:31 +0330

    cs

--- solver.py
+++ solver.py
@@ -181,7 +181,7 @@ class Solver(object):
                 self.optimizer.step()
 
             vali_loss1, vali_loss2 = self.vali(self.test_loader)
-            # TESTTETSTTETETSTTTSTTETETTETTETETETTEETETETETETTETEETETETETETETTETETETETETETETETETTETE
+            # TWESTTETSTTETETSTTTSTTETETTETTETETETTEETETETETETTETEETETETETETETTETETETETETETETETETTETE
             writer.add_scalar("Loss/train", loss, self.num_epochs)
             print(
                 "Epoch: {0}, Cost time: {1:.3f}s ".format(
